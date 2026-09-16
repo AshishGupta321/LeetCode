4c45a824ae184361d867ce58b3d73adeeecc76cf
AshishGupta321 <ashishgupta09106@gmail.com>
2026-09-16 23:09:54 +0530

    Time: 0 ms (100%), Space: 9.3 MB (82.92%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int mapST[256]={0};
+        int mapTS[256]={0};
+        for(int i=0;i<s.size();i++){
+            if(mapST[s[i]] !=0 && mapST[s[i]]!=t[i])
+            return false;
+            if(mapTS[t[i]] !=0 && mapTS[t[i]]!=s[i])
+            return false;
+            mapST[s[i]]=t[i];
+            mapTS[t[i]]=s[i];
+        }
+        return true;
+    }
+};
